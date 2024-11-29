f4d861dae75b21cd75aeba10d5fbb60f9897cfee
yensen-arch <officialdevain25@gmail.com>
2024-11-29 14:19:27 +0530

    TermsModal improved

--- src/components/TermsModal.js
+++ src/components/TermsModal.js
@@ -32,19 +32,28 @@ const features = [
 ];
 
 export default function FeatureModal() {
-  const [isVisible, setIsVisible] = useState(true);
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [isAnimating, setIsAnimating] = useState(true);
 
   useEffect(() => {
     const hasSeenModal = localStorage.getItem("hasSeenFeaturesModal");
     if (hasSeenModal) {
-      setIsVisible(false);
+      setIsAnimating(false);
     }
   }, []);
 
+  useEffect(() => {
+    if (currentIndex < features.length - 1) {
+      const timer = setInterval(() => {
+        setCurrentIndex((prevIndex) => prevIndex + 1);
+      }, 4000);
+      return () => clearInterval(timer);
+    }
+  }, [currentIndex]);
+
   const hideModal = () => {
-    setIsVisible(false);
     localStorage.setItem("hasSeenFeaturesModal", "true");
+    setIsAnimating(false);
   };
 
   const nextSlide = () => {
@@ -55,79 +64,90 @@ export default function FeatureModal() {
     setCurrentIndex(index);
   };
 
-  if (!isVisible) return null;
-
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 p-4 sm:p-6 md:p-8">
-      <div className="bg-white rounded-2xl shadow-xl w-full max-w-4xl overflow-hidden">
-        <div className="relative p-4 sm:p-6 md:p-8">
-          {currentIndex < features.length - 1 && (
-            <button
-              onClick={hideModal}
-              className="absolute top-4 right-4 text-black rounded-full px-3 py-1 bg-slate-300 z-10 text-sm sm:text-base transition-colors duration-300 hover:bg-slate-400"
-            >
-              SKIP
-            </button>
-          )}
-          <div className="h-[calc(100vh-8rem)] max-h-[36rem] overflow-hidden relative">
-            <AnimatePresence initial={false} custom={currentIndex}>
-              <motion.div
-                key={currentIndex}
-                custom={currentIndex}
-                initial={{ opacity: 0, x: "100%" }}
-                animate={{ opacity: 1, x: 0 }}
-                exit={{ opacity: 0, x: "-100%" }}
-                transition={{ type: "", stiffness: 300, damping: 30 }}
-                className="absolute inset-0 flex flex-col md:flex-row items-center"
-              >
-                <div className="w-full md:w-1/2 h-48 sm:h-64 md:h-full flex justify-center items-center p-4">
-                  <img
-                    src={features[currentIndex].image}
-                    alt={features[currentIndex].title}
-                    className="object-contain h-full w-full "
+    <AnimatePresence>
+      {isAnimating && (
+        <motion.div
+          initial={{ opacity: 0, x: 0 }}
+          animate={{ opacity: 1, x: 0 }}
+          exit={{ opacity: 0, x: "-100%" }}
+          transition={{ type: "spring", stiffness: 300, damping: 30 }}
+          onAnimationComplete={(definition) => {
+            if (definition === "exit") {
+              setIsAnimating(false); // Remove modal from DOM after exit animation
+            }
+          }}
+          className="fixed inset-0 flex items-center justify-center z-50 p-4 sm:p-6 md:p-8"
+        >
+          <div className="bg-white rounded-2xl shadow-xl w-full max-w-4xl overflow-hidden">
+            <div className="relative p-4 sm:p-6 md:p-8">
+              {currentIndex < features.length - 1 && (
+                <button
+                  onClick={hideModal}
+                  className="absolute top-4 right-4 text-black rounded-full px-3 py-1 bg-slate-300 z-10 text-sm sm:text-base transition-colors duration-300 hover:bg-slate-400"
+                >
+                  SKIP
+                </button>
+              )}
+              <div className="h-[calc(100vh-8rem)] max-h-[36rem] overflow-hidden relative">
+                <AnimatePresence initial={false} custom={currentIndex}>
+                  <motion.div
+                    key={currentIndex}
+                    custom={currentIndex}
+                    initial={{ opacity: 0, x: "100%" }}
+                    animate={{ opacity: 1, x: 0 }}
+                    exit={{ opacity: 0, x: "-100%" }}
+                    transition={{ type: "spring", stiffness: 300, damping: 30 }}
+                    className="absolute inset-0 flex flex-col md:flex-row items-center"
+                  >
+                    <div className="w-full md:w-1/2 h-48 sm:h-64 md:h-full flex justify-center items-center p-4">
+                      <img
+                        src={features[currentIndex].image}
+                        alt={features[currentIndex].title}
+                        className="object-contain h-full w-full "
+                      />
+                    </div>
+                    <div className="w-full md:w-1/2 text-center md:text-left h-auto md:h-full flex flex-col justify-center p-4 md:p-8">
+                      <h2 className="text-xl sm:text-2xl md:text-3xl font-bold mb-4 text-purple-800">
+                        {features[currentIndex].title}
+                      </h2>
+                      <p className="text-gray-600 text-sm sm:text-base md:text-lg mb-6">
+                        {features[currentIndex].description}
+                      </p>
+                      {currentIndex < features.length - 1 ? (
+                        <button
+                          onClick={nextSlide}
+                          className="w-full md:w-auto px-6 py-3 font-bold bg-purple-800 text-white rounded-lg transition-colors duration-300 hover:bg-purple-900 text-sm sm:text-base"
+                        >
+                          CONTINUE
+                        </button>
+                      ) : (
+                        <button
+                          onClick={hideModal}
+                          className="w-full md:w-auto px-6 py-3 font-bold bg-purple-800 text-white rounded-lg transition-colors duration-300 hover:bg-purple-900 text-sm sm:text-base"
+                        >
+                          CLOSE
+                        </button>
+                      )}
+                    </div>
+                  </motion.div>
+                </AnimatePresence>
+              </div>
+              <div className="mt-4 flex justify-center space-x-2">
+                {features.map((_, index) => (
+                  <button
+                    key={index}
+                    onClick={() => goToSlide(index)}
+                    className={`w-2 h-2 sm:w-3 sm:h-3 rounded-full transition-colors duration-300 ${
+                      index === currentIndex ? "bg-purple-800" : "bg-gray-300"
+                    }`}
                   />
-                </div>
-                <div className="w-full md:w-1/2 text-center md:text-left h-auto md:h-full flex flex-col justify-center p-4 md:p-8">
-                  <h2 className="text-xl sm:text-2xl md:text-3xl font-bold mb-4 text-purple-800">
-                    {features[currentIndex].title}
-                  </h2>
-                  <p className="text-gray-600 text-sm sm:text-base md:text-lg mb-6">
-                    {features[currentIndex].description}
-                  </p>
-                  {currentIndex < features.length - 1 ? (
-                    <button
-                      onClick={nextSlide}
-                      className="w-full md:w-auto px-6 py-3 font-bold bg-purple-800 text-white rounded-lg transition-colors duration-300 hover:bg-purple-900 text-sm sm:text-base"
-                    >
-                      CONTINUE
-                    </button>
-                  ) : (
-                    <button
-                      onClick={hideModal}
-                      className="w-full md:w-auto px-6 py-3 font-bold bg-purple-800 text-white rounded-lg transition-colors duration-300 hover:bg-purple-900 text-sm sm:text-base"
-                    >
-                      CLOSE
-                    </button>
-                  )}
-                </div>
-              </motion.div>
-            </AnimatePresence>
+                ))}
+              </div>
+            </div>
           </div>
-          <div className="mt-4 flex justify-center space-x-2">
-            {features.map((_, index) => (
-              <button
-                key={index}
-                onClick={() => goToSlide(index)}
-                className={`w-2 h-2 sm:w-3 sm:h-3 rounded-full transition-colors duration-300 ${
-                  index === currentIndex
-                    ? "bg-purple-800"
-                    : "bg-gray-300 hover:bg-gray-400"
-                }`}
-              />
-            ))}
-          </div>
-        </div>
-      </div>
-    </div>
+        </motion.div>
+      )}
+    </AnimatePresence>
   );
 }
